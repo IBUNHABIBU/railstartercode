5be9eeeeb4c26ce9d95b4ead1074921039a08fe6
IBUNHABIBU <ibunhabib@gmail.com>
2023-02-10 20:46:38 +0300

    Add authenticate method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: { case_sensitive: false },
                     format: /\A\S+@\S+\z/
+
+    def self.authenticate(email, password)
+        user = User.find_by(email: email)
+        user && user.authenticate(password)
+    end
 end
