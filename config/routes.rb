f49895b849c949a692697faf685801a76b91be7f
IBUNHABIBU <ibunhabib@gmail.com>
2023-02-10 21:19:14 +0300

    Add bcrypt gem

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  get 'sessions/new'
+  get 'signup', to: "users#new"
+  resources :users
   root "home#index"
 end
