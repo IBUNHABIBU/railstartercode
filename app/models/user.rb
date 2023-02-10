24fc592e5d2d1c788cfd8f5f2c5b65039b367421
IBUNHABIBU <ibunhabib@gmail.com>
2023-02-10 20:32:16 +0300

    Add user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false},
+                format: /\A\S+@\S+\z/
 end
