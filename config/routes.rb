87f056783834d5fb0890a83441a66423983f54d6
IBUNHABIBU <ibunhabib@gmail.com>
2023-02-13 22:44:51 +0300

    Redefine the routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   # get 'sessions/new'
   get 'signup', to: "users#new"
   resources :users
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   root "home#index"
 end
